68575dfc0a327494286cbd73a63c022a1bdb7198
cmabry1 <143213754+cmabry1@users.noreply.github.com>
2026-02-01 14:58:53 -0600

    Add Inventory class with product management methods

---
+++ Inventory.h
@@ -0,0 +1,30 @@
+#ifndef INVENTORY_H
+#define INVENTORY_H
+
+#include <vector>
+#include "Products.h"
+
+class Inventory {
+    vector<Product*> products;
+
+public:
+    void addProduct(Product* p) {
+        products.push_back(p);
+    }
+
+    void displayProducts() {
+        for (auto p : products) {
+            p->getDetails();
+        }
+    }
+
+    Product* search(string name) {
+        for (auto p : products) {
+            if (p->getName() == name)
+                return p;
+        }
+        return nullptr;
+    }
+};
+
+#endif
