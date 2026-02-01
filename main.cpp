ed29037c1de8018c3be5519a41cc249624eb892c
cmabry1 <143213754+cmabry1@users.noreply.github.com>
2026-02-01 15:07:41 -0600

    Clean up comments in main.cpp
    Removed comments for product creation and inventory addition.

--- main.cpp
+++ main.cpp
@@ -4,12 +4,10 @@
 int main() {
     Inventory store;
 
-    // Create products
     Product* v1 = new Volleyball(1, "Pro Volleyball", 25.0, 10, "Leather", "Official");
     Product* t1 = new Tent(2, "Camping Tent", 120.0, 5, "4-Person", "Rain Resistant");
     Product* s1 = new SoccerBall(3, "Soccer Ball", 30.0, 20, "Synthetic");
 
-    // Add to inventory
     store.addProduct(v1);
     store.addProduct(t1);
     store.addProduct(s1);
@@ -18,9 +16,8 @@ int main() {
     store.displayProducts();
 
     Cart cart;
-
-    cout << "\n--- Shopping ---\n";
     Product* found = store.search("Soccer Ball");
+
     if (found)
         cart.addToCart(found, 2);
 
